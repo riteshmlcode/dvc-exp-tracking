9098d1c09872faa0306dd333bf7bab1060b285f6
xcode <xcode@xcode.com>
2025-01-16 01:28:08 +0530

    Best Experiments

--- src/model_training.py
+++ src/model_training.py
@@ -15,8 +15,8 @@ y = df['Placed']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-n_estimators = 20
-max_depth = 15
+n_estimators = 30
+max_depth = 1
 
 rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
 rf.fit(X_train,y_train)
